d037d3aea24957f597ba078ca04e7fde98e8ed3a
NerijusU <86874206+NerijusU@users.noreply.github.com>
2022-01-18 10:18:31 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Meme from "./Meme";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Meme Generator</h1>
+      <Meme />
+    </div>
+  );
+}
